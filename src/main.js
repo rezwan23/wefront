b6429f434bb55ebe3548d0df5abfcd90ccf4f1fc
Razwanul <razwanulghani@gmail.com>
2021-07-29 12:20:08 +0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+const axios = require('axios');
+
+// axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.currentUser.token}`;
+
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:8080';
+
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted(){
+    axios.get('http://backend.test/api/test').then(res=>console.log(res.data));
+  }
+}
+</script>
